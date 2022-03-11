3d3a842f6e93bcb8d281ea017fe6c74e2d43debb
Adil Belhachemi <adilb@Adils-MacBook-Pro.local>
2022-03-11 12:50:16 -0500

    initial blockchain

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"golang-blockchain/blockchain"
+	"strconv"
+)
+
+func main() {
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First block after Genesis")
+	chain.AddBlock("Second block after Genesis")
+	chain.AddBlock("Third block after Genesis")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Data in block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println("-----------------------------------")
+	}
+}
